7a795421413c2ec7d18938996e5ff66475a553c3
Nikolaos Plastiras <nipla31@gmail.com>
2025-01-28 11:31:36 +0100

    Added 2023 eras

--- python/producers/leptonSFProducer.py
+++ python/producers/leptonSFProducer.py
@@ -7,7 +7,7 @@ ROOT.PyConfig.IgnoreCommandLineOptions = True
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 
 
-era_dict = {"2021": '2022_Summer22', "2022": '2022_Summer22EE'}
+era_dict = {"2022": '2022_Summer22', "2022EE": '2022_Summer22EE', "2023": '2023_Summer23', "2023BPix": '2023_Summer23BPix'}
 
 class ElectronSFProducer(Module, object):
 
