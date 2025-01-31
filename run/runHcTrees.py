acc290a7ce85533841b302e1d14645dfc374bc5c
Nikolaos Plastiras <nipla31@gmail.com>
2025-01-31 12:41:17 +0100

    Updated JSON paths for the split samples

--- run/runHcTrees.py
+++ run/runHcTrees.py
@@ -24,7 +24,9 @@ args = parser.parse_args()
 
 golden_json = {
     '2022': 'Cert_Collisions2022_355100_362760_Golden.json',
-    '2023': 'Cert_Collisions2023_366442_370790_Golden.json'
+    '2022EE': 'Cert_Collisions2022_355100_362760_Golden.json',
+    '2023': 'Cert_Collisions2023_366442_370790_Golden.json',
+    '2023BPix': 'Cert_Collisions2023_366442_370790_Golden.json'
 }
 
 ## read samples yaml file and produce json file to be used by condor
