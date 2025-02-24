b8cb8323860e0a6eae1ff25195d6f418828affe7
Nikolaos Plastiras <nipla31@gmail.com>
2025-02-24 11:36:41 +0100

    Removed JSON files for Run3

--- run/runHcTrees.py
+++ run/runHcTrees.py
@@ -26,11 +26,7 @@ golden_json = {
     '2016APV': 'Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt',
     '2016': 'Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt',
     '2017': 'Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt',
-    '2018': 'Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt',
-    '2022': 'Cert_Collisions2022_355100_362760_Golden.json',
-    '2022EE': 'Cert_Collisions2022_355100_362760_Golden.json',
-    '2023': 'Cert_Collisions2023_366442_370790_Golden.json',
-    '2023BPix': 'Cert_Collisions2023_366442_370790_Golden.json'
+    '2018': 'Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt'
 }
 
 ## read samples yaml file and produce json file to be used by condor
