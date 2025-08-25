951911168943d6789ffb5abbeae4cfbefb0de47b
Nikolaos Plastiras <nipla31@gmail.com>
2025-08-25 19:55:26 +0200

    Updated the jetvetomap module

--- python/producers/jetSFProducer.py
+++ python/producers/jetSFProducer.py
@@ -17,7 +17,13 @@ key_dict = {
     
 class JetVMAPProducer(Module, object):
     def __init__(self, year, dataset_type, **kwargs):
-        """According to JetMet POG:Module to apply veto maps that veto out events with important jets in the "hot" or "cold" zones. These maps should be applied similarly both on Data and MC, to keep the phase-spaces equal. https://cms-jerc.web.cern.ch/Recommendations/#jet-veto-maps
+        """According to JetMet POG:
+           These are the jet veto maps showing regions with an excess of jets (hot zones) and lack of jets
+           (cold zones). Using the phi-symmetry of the CMS detector, these areas with detector and or calibration issues can be pinpointed.
+           This module applies veto maps that veto out events with important jets in these "hot" or "cold" zones. 
+           These maps should be applied similarly both on Data and MC, to keep the phase-spaces equal.
+           Non-zero value indicates that the region is vetoed 
+           https://cms-jerc.web.cern.ch/Recommendations/#jet-veto-maps
         """
 
         self.year = year
@@ -29,7 +35,7 @@ class JetVMAPProducer(Module, object):
 
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.out = wrappedOutputTree
-        self.out.branch("Flag_JetVetoed", "I", title="Event veto flag from Jet Veto Map")
+        self.out.branch("Flag_JetVetoed", "O", title="Event veto flag from Jet Veto Map")
 
     def fixPhi(self, phi):
         epsilon = 1e-6  # Small offset to avoid boundary issues
@@ -51,7 +57,7 @@ class JetVMAPProducer(Module, object):
         - jets that don't overlap with PF muon (dR < 0.2)
         """
         jets = Collection(event, "Jet")
-        veto_flag = 0
+        veto_flag = False 
         for i,jet in enumerate(jets):
             if (jet.pt> 15 and (jet.jetId ==2 or jet.jetId ==6) and jet.chEmEF < 0.9 and jet.neEmEF <0.9 and jet.muonIdx1 == -1 and jet.muonIdx2 == -1):
             
@@ -61,7 +67,7 @@ class JetVMAPProducer(Module, object):
 
                 # Check if the jet is vetoed
                 if veto_map_value > 0:
-                    veto_flag = 1  # Set flag if a vetoed jet is found
+                    veto_flag = True  # Set flag if a vetoed jet is found
                     break  # Break out of the loop since we only need one veto to trigger
 
         # Fill the branch with the veto result
