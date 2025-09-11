403d3c3e76a56e93d38b0830eb204d5b7d58a1a6
Nikolaos Plastiras <nipla31@gmail.com>
2025-09-11 15:17:58 +0200

    Fixed merging multiple subsamples

--- run/runHcTrees.py
+++ run/runHcTrees.py
@@ -381,7 +381,7 @@ def run_add_weights():
     for sample, process_files in sample_dirs.items():
         if len(process_files) > 1:
             final_merged_file = os.path.join(base_output_dir, dataset_type, year, sample, f"{sample}_final_merged.root")
-            merge_cmd = f"haddnano.py {final_merged_file} {' '.join(process_files)}"
+            merge_cmd = f"hadd {final_merged_file} {' '.join(process_files)}"
             print(f"Merging {len(process_files)} weighted files into {final_merged_file}")
             subprocess.run(merge_cmd, shell=True, check=True)
         elif process_files:
